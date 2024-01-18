bd42afb0b3ba08aeeda85e34e0d09ac6c9472bd1
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2024-01-18 12:59:02 +0530

    added toString

--- src/main/java/com/level11data/databricks/client/entities/permissions/PermissionInfoDTO.java
+++ src/main/java/com/level11data/databricks/client/entities/permissions/PermissionInfoDTO.java
@@ -3,6 +3,9 @@ package com.level11data.databricks.client.entities.permissions;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.level11data.databricks.permissions.PermissionLevel;
 
 import javax.ws.rs.Consumes;
@@ -22,4 +25,13 @@ public class PermissionInfoDTO {
     public PermissionLevel PermissionLevel;
 
 
+    @Override
+    public String toString() {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        try {
+            return ow.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            return "Could Not Marshal Object to JSON";
+        }
+    }
 }
